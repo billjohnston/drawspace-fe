781bafbcbd9b4a5f87dc796a9ba4c189f02bf974
Bill Johnston <billjohnston@pm.me>
2020-11-07 20:09:28 +0000

    404 fix

---
+++ components/Page404.tsx
@@ -0,0 +1,9 @@
+import PageError from 'components/PageError'
+
+import { FunctionComponent } from 'react'
+
+const Page404: FunctionComponent = () => {
+    return <PageError error="This page could not be found"/>
+}
+
+export default Page404
